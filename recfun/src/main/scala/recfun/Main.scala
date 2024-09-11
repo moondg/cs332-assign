8e1d0515ffb572f0462f1f670a9660433b1f2acf
moondg <moondg07@outlook.com>
2024-09-11 19:57:22 +0900

    feat: complete assn1

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,30 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0 || r == 0 || c == r) 1
+    else pascal(c,r-1)+pascal(c-1,r-1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def isBalance(parens: List[Char], lparenStackSize: Int): Boolean = {
+      if (parens.isEmpty) lparenStackSize == 0
+      else if (parens.head == '(') isBalance(parens.tail, lparenStackSize + 1)
+      else if (lparenStackSize == 0) false
+      else isBalance(parens.tail, lparenStackSize - 1)
+    }
+
+    isBalance(chars.filter(x => x == '(' || x == ')'), 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money < 0) 0
+    else if (coins.isEmpty) if (money == 0) 1 else 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
 }
